1c762262ea509ad691e3ab589c19a0026475abc8
dashengju <dashengju@qq.com>
2014-12-12 22:49:10 +0800

    add Seriable

--- external/storm-hbase/src/main/java/org/apache/storm/hbase/trident/mapper/TridentTupleMapper.java
+++ external/storm-hbase/src/main/java/org/apache/storm/hbase/trident/mapper/TridentTupleMapper.java
@@ -2,10 +2,12 @@ package org.apache.storm.hbase.trident.mapper;
 
 import storm.trident.tuple.TridentTuple;
 
+import java.io.Serializable;
+
 /**
  * Created by judasheng on 14-12-11.
  */
-public interface TridentTupleMapper {
+public interface TridentTupleMapper extends Serializable {
     public String getKeyFromTridentTuple(TridentTuple tuple);
     public Object getValueFromTridentTuple(TridentTuple tuple);
 }
